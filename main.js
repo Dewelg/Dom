79bd78d32ed2a0fa843dcac0d2f04b870f1a8bff
_Dewelg_ <_(dewel713@gmail.com_)>
2022-10-16 22:58:42 -0400

    working on it

--- main.js
+++ main.js
@@ -7,25 +7,34 @@ let node1 = document.getElementById("node1");
 node1.textContent = "I used the getElementById(node1) method to access this";
 
 // Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
-let node2 = document.getElementsByClassName("node2");
+let node2 = document.getElementsByClassName("node2")[0];
 
 node2.textContent = "I used the getElementByClassName(node2) method to access this";
-let parent = document.querySelector("p");
-
 
 // Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+let h3s = document.getElementsByTagName("h3");
 
+for (let h3 of h3s) {
+  h3.textContent =
+    'I used the getElementByTagName("h3") method to access all of these';
+}
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
-
 // TODO: Append the created node to the parent node using the element.appendChild() method
-
+let p = document.createElement('p');
+let parent = document.querySelector("#parent");
+p.textContent = "This node was created using the createElement() method";
+parent.appendChild(p);
 // TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
+// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+//let a = document.createTextNode('I am a <a> tag');
 
 // BONUS: Add a link href to the <a>
-
-// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+let link = document.createElement('a');
+link.textContent = "W3";
+link.href = "http://www.w3.org/2000/svg"
+parent.insertBefore(link,p);
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
